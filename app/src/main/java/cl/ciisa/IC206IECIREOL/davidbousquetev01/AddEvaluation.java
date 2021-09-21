5430a6e8c0aa723dc421120dcf6fa90af9157382
programaciondb <david@programaciondb.cl>
2021-09-20 23:06:02 -0300

    first commit, all views created with no functionality

---
+++ app/src/main/java/cl/ciisa/IC206IECIREOL/davidbousquetev01/AddEvaluation.java
@@ -0,0 +1,14 @@
+package cl.ciisa.IC206IECIREOL.davidbousquetev01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEvaluation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_evaluation);
+    }
+}
---
+++ app/src/main/java/cl/ciisa/IC206IECIREOL/davidbousquetev01/EvaluationsActivity.java
@@ -0,0 +1,14 @@
+package cl.ciisa.IC206IECIREOL.davidbousquetev01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EvaluationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluations);
+    }
+}
